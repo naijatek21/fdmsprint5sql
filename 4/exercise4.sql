ed04c2a023fdbdb453173bfa730c6dc6a7deda4c
Chike Egbuchulam <31557778+naijatek21@users.noreply.github.com>
2026-04-14 16:29:58 -0400

    typed out

--- 4/exercise4.sql
+++ 4/exercise4.sql
@@ -1,24 +1,120 @@
 USE freelance;
 SET AUTOCOMMIT=0;
 
+--2
+
 CREATE TABLE locations_copy LIKE locations;
 
-Insert INTO locations_copy (place_id, city, country) 
-    Select place_id, city, country 
-    from locations;
+INSERT INTO locations_copy (place_id, city, country) 
+    SELECT place_id, city, country 
+    FROM locations;
 
 SELECT * FROM locations;
 
 Create TABLE clients_copy LIKE clients;
 
-Insert Into clients (client_id, client_name, place_id) 
+INSERT INTO clients (client_id, client_name, place_id) 
     SELECT client_id, client_name, place_id 
     FROM clients;
 
 SELECT * FROM clients_copy;
 
 Create TABLE contractors_copy LIKE contractors;
-Insert Into contractors (contractor_id, first_name, last_name, client_id)  SELECT contractor_id, first_name,last_name, client_id from contractors;
+INSERT INTO contractors (contractor_id, first_name, last_name, client_id)  SELECT contractor_id, first_name,last_name, client_id from contractors;
 
 SELECT * FROM contractors_copy;
+
+--3.1
+
+SET SQL_SAFE_UPDATES=0;
+UPDATE clients 
+SET place_id = 2
+WHERE client_name="Toyota"
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT client_name,place_id 
+FROM clients 
+WHERE client_name = 'Toyota' OR client_name='IBM' 
+--3.2
+
+SET SQL_SAFE_UPDATES=0;
+UPDATE clients 
+SET place_id = (
+    SELECT place_id 
+    FROM clients 
+    WHERE client_name='Toyota')
+WHERE client_name="Toyota"
+SET SQL_SAFE_UPDATES = 1;
+--test
+SELECT client_name,place_id 
+FROM clients 
+WHERE client_name = 'Toyota' OR client_name='IBM' 
+
+
+--4.1
+SET SQL_SAFE_UPDATES=0;
+UPDATE contractors 
+SET 
+    first_name='Jonathan',
+    last_name = 'Smithson'
+WHERE first_name = 'John' AND last_name = 'Smith'
+SET SQL_SAFE_UPDATES = 1;
+ 
+ --4.3
+    UPDATE contractors 
+    SET 
+        first_name='Jonathan',
+        last_name = 'Smithson'
+    WHERE contractor_id = 7
+    COMMIT
+    SET AUTOCOMMIT=1
+
+--4.4
+     UPDATE contractors 
+    SET 
+        first_name='Jonathan',
+        last_name = 'Smithson'
+    WHERE contractor_id = (
+        SELECT contractor_id 
+        FROM contractors
+        WHERE first_name = 'John' AND last_name = 'Smith'
+    )
+
+--4.5
+--      UPDATE contractors 
+--     SET 
+--         first_name='Jonathan',
+--         last_name = 'Smithson'
+--     WHERE contractor_id = (
+--         SELECT contractor_id 
+--         FROM contractors
+--         WHERE first_name = 'John' 
+--     )
+  
+
+--5
+    DELETE FROM contactors
+        WHERE client_id = (
+            SELECT client_id 
+            FROM clients 
+            WHERE client_name = 'EDF'
+            );
+    DELETE FROM clients
+        WHERE client_name = 'EDF'
+
+--6
+    DELETE FROM contractors
+        WHERE client_id = (
+            SELECT client_id 
+            FROM clients 
+            WHERE client_name = 'Google'
+            );
+
+
+--7
+    DELETE FROM clients
+        WHERE client_id NOT IN (
+            SELECT client_id 
+            FROM contractors
+        )
 
