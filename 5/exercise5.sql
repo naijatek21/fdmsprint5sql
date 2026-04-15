9f9585288c1326903cdf97c9cc5a8b7b4c2cccb8
Chike Egbuchulam <31557778+naijatek21@users.noreply.github.com>
2026-04-15 14:26:20 -0400

    finished ex 5

--- 5/exercise5.sql
+++ 5/exercise5.sql
@@ -72,3 +72,43 @@
     WHERE stock_ex_id = 3 AND share_amount > 20000;
     
     #Result : 5 trades with ids: 10,24,38,53,61
+
+#Q12
+    SELECT broker_id
+    FROM broker_stock_ex
+    WHERE stock_ex_id = 2
+    ORDER BY broker_id ASC;
+
+    #Result : 1,4,5,6,7,11,13,15
+
+#Q13
+    SELECT * 
+    FROM currencies
+    WHERE currency_name LIKE '%Dollar%';
+
+    #Result: US Dollar, Australian Dollar
+
+#Q14
+    SELECT stock_ex_name,stock_ex_symbol
+    FROM stock_exchanges
+    WHERE stock_ex_symbol LIKE '%SE%'
+    ORDER BY stock_ex_name DESC;
+
+    /*Result:
+	    Tokyo Stock Exchange
+        New York Stock Exchange
+        Moscow Stock Exchange
+		London Stock Exchange
+        
+    */
+
+#Q15
+	SELECT DISTINCT share_id 
+	FROM shares_prices
+	WHERE price >= 400 AND price <= 500;
+    #Result: 1 and 7
+	
+#Q16
+	SELECT trade_id, (price_total/share_amount) AS Cost_Per_Share
+    FROM trades;
+    
